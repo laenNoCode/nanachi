2f0b2115bae78ebe04d66e92e86ac57f90ec7396
Mathieu CAROFF <mathieu.caroff@free.fr>
2018-12-07 05:33:34 +0100

    (site): Move files into public

---
+++ public/mainSketch.js
@@ -0,0 +1,128 @@
+import Vipi from './vipi.js';
+import BasicEnemy from './basicEnemy.js';
+
+var hero;
+var enemies;
+var baseWidth = 480;
+var baseHeight = 270;
+var scaleX;
+var scaleY;
+var skyHeight = 5;
+var groundHeight = 10;
+var score = 0;
+var basicSpeed = 1;
+const ENEMY_TYPES = {
+    TORNADO: 0,
+    CACTUS: 1,
+    SNAKE: 2,
+    VULTURE: 3,
+}
+const MIN_FRAME_GEN_WAIT = 50;
+const MAX_FRAME_GEN_WAIT = 250;
+var framesBeforeSpawn = 1;
+
+
+window.setup = function() {
+    scaleX = 1.0;
+    scaleY = 1.0;
+
+    createCanvas(baseWidth * scaleX, baseHeight * scaleY);
+    frameRate(30);
+    hero = new Vipi(30, 50, 0, 0.13, 0.9, 0.99, 1, 32, 32);
+    enemies = [];
+}
+
+window.draw = function() {
+    if (hero.alive) {
+        window.update();
+        background(255, 100, 200);
+        hero.draw(frameCount, scaleX, scaleY);
+        fill(255);
+        text(floor(score), 1 * scaleX, (baseHeight) * scaleY - 1);
+        for (var v of enemies) {
+            v.draw(frameCount, scaleX, scaleY);
+        }
+    } else {
+        if (frameCount - hero.frame < 20) {
+            background(0, 0, 0, 40);
+        } else {
+            if (frameCount - hero.frame == 20) {
+                background(0);
+            }
+            //255,235,150
+            fill(255, 235, 150, 20);
+            textSize(32);
+            text(" Score : " + floor(score), baseWidth / 4, baseHeight / 4);
+
+        }
+    }
+
+}
+window.update = function() {
+    if (hero.alive) {
+        var currentSpeed = 1 + pow(frameCount / 60.0, 0.75)
+        score += currentSpeed / 30.0;
+        hero.update(baseHeight, groundHeight, skyHeight, currentSpeed, frameCount);
+        for (var v = enemies.length - 1; v >= 0; v--) {
+            enemies[v].update(currentSpeed, hero);
+
+        }
+        var newEnemies = [];
+        for (var v of enemies) {
+            if (v.out(baseHeight))
+                newEnemies.push(v);
+        }
+        framesBeforeSpawn -= currentSpeed;
+
+        if (framesBeforeSpawn <= 0) {
+            generateEnemy();
+        }
+
+    }
+}
+
+
+window.generateEnemy = function() {
+    var enemyType = floor(Math.random() * 4);
+    switch (enemyType) {
+        case (ENEMY_TYPES.CACTUS):
+            enemies.push(new BasicEnemy(baseWidth, baseHeight - groundHeight - 64, -basicSpeed, 0, 32, 64));
+            console.log("cactus spawned");
+            break;
+        case (ENEMY_TYPES.SNAKE):
+            enemies.push(new BasicEnemy(baseWidth, baseHeight - groundHeight - 64, -basicSpeed, 0, 32, 64));
+
+            break;
+        case (ENEMY_TYPES.TORNADO):
+            enemies.push(new BasicEnemy(baseWidth, skyHeight + random() * (baseHeight), -basicSpeed, 0, 48, 64));
+            break;
+        case (ENEMY_TYPES.VULTURE):
+            var endHeight = random() * baseHeight;
+            var distance = sqrt((endHeight - skyHeight) * (endHeight - skyHeight) + (baseWidth) * (baseWidth));
+
+            enemies.push(new BasicEnemy(baseWidth, skyHeight, -basicSpeed * 2 * baseWidth / distance, (endHeight - skyHeight) / distance, 37, 24));
+            break;
+    }
+    framesBeforeSpawn = floor(random() * (MAX_FRAME_GEN_WAIT - MIN_FRAME_GEN_WAIT)) + MIN_FRAME_GEN_WAIT;
+}
+
+window.handlePressed = function() {
+    hero.onUpPressed();
+}
+window.handleReleased = function() {
+    hero.onUpReleased();
+
+}
+
+window.keyPressed = function() {
+    handlePressed();
+}
+window.keyReleased = function() {
+    handleReleased();
+}
+window.touchStarted = function() {
+    handlePressed();
+}
+window.touchEnded = function() {
+    handleReleased();
+}
